e299ce9bfe5bc713dce4ebe0c07a78911e792c3e
casmirCode <cachu7@student.monash.edu>
2019-08-26 09:03:27 +0200

    added css update

--- router.js
+++ router.js
@@ -1,35 +1,22 @@
 let express = require('express');
 let router = express.Router();
-
 let db = [];
-db.push({
-  carId: 0,
-  carMake: 'BMW',
-  carModel: '735',
-  carYear: 2014
-});
-db.push({
-  carId: 1,
-  carMake: 'Mercedes',
-  carModel: 'C250',
-  carYear: 2017
-});
-db.push({
-  carId: 3,
-  carMake: 'Audi',
-  carModel: 'A6',
-  carYear: 2019
-});
 
 router.get('/', function (req, res) {
-  let randomId = Math.round(Math.random() * 100);
   res.render('index.html', {
-    username: "msa",
-    carDb: db,
-    id: randomId
+    username: "Guest"
   });
 })
 
+router.post('/newItem', function(req,res){
+    db.push(req.body);
+    
+})
 
+router.get('/listtask.html', function (req, res) {
+  res.render('listtask.html', {
+    taskDb: db
+  });
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 let express = require('express');
 const app = express();
 let router = require('./router.js');
-
+let bodyParser = require('body-parser');
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({
+  extended:false
+}))
+// app.use(bodyParser.json())
 app.use('/', router);
 app.use(express.static('images'));
 app.use(express.static('css'));
+app.use(express.static('views'));
 
 
 app.listen(8080, () => {
